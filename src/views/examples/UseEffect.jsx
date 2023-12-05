b21a0d0d35f4933b8b08d3fccc3bd09d9101f77b
matheusadami.lima@gmail.com <matheusadami.lima@gmail.com>
2023-12-05 16:14:19 -0300

    useref

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
+
+
+function factory(n){
+    if(n < 0) return -1
+    if(n < 1) return 1
+    return n * factory(n - 1);
+}
+
+function parOuimpar(n){
+    return n % 2 === 0 ? 'Par' : 'Impar'
+}
+
+
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
+    const [parouimpar, setParouImpar] = useState('');
+
+    useEffect( () => {
+        setParouImpar(parOuimpar(number))
+    },[number]);
+
+    useEffect( () => {
+        setFatorial(factory(number))
+    },[number]);
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
-        </div>
+            <SectionTitle title="Exercicio 01" />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red"> {fatorial < 0 ? 'Não existe Fatorial' : fatorial}</span>
+                </div>
+            <input type="number" className="input" value={number} onChange={e => setNumber(e.target.value)}/>
+            </div>
+            <SectionTitle title="Exercicio 01" />
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red"> {parouimpar}</span>
+                </div>
+            </div>
+       </div>
     )
 }
 
--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+    const [value1, setvalue1] = useState('');
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <input type="text" className="input" value={value1} onChange={e => setvalue1(e.target.value)}/>    
+            </div>    
         </div>
     )
 }
